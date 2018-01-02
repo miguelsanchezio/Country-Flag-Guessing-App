784ec05029e9db6c5903d3732f577e08a889a1ea
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-01-02 09:11:14 -0800

    Refactored code, guess inputs are now in the Inputs component (2)

---
+++ client/src/components/Inputs.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Inputs extends Component {
+    render() {
+        const { countries, gameStatus, onGuessChange } = this.props;
+
+        const countryInputs = countries.map(country => {
+            return (
+                <div
+                    key={country.name}
+                    className='form-check form-check-inline'
+                >
+                    <label className='form-check-label'>
+                        <input
+                            className='form-check-input'
+                            type='radio'
+                            value={country.name}
+                            name='countryGuess'
+                            onChange={onGuessChange}
+                        />
+                        {country.name}                    
+                    </label>
+                </div>
+            )
+        })
+
+        return (
+            <div>
+                {countryInputs}
+            </div>
+        )
+    }
+}
+
+export default Inputs;
